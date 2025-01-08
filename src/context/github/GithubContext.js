aa80faa1073caa0e674cd877f5bccf59de2b34cd
rok <rok@gmail.com>
2025-01-08 16:18:50 +0900

    컨텍스트를 이용한 유저검색기능 제작

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,24 +8,25 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
-  //테스트 유저 리스트
-  const fetchUsers = () => {
+  //키워드로 유저찾기
+  const searchUsers = (text) => {
     setLoading(true);
-    fetch(`${GITHUB_URL}/users`, {
+    const params = new URLSearchParams({ q: text }); //쿼리파라미터 설정
+    fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `Bearer ${GITHUB_TOKEN}`,
       },
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        setUsers(data);
+        console.log(data.items);
+        setUsers(data.items);
         setLoading(false);
       })
       .catch((err) => console.log(err));
   };
   return (
-    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+    <GithubContext.Provider value={{ users, loading, searchUsers }}>
       {children}
     </GithubContext.Provider>
   );
